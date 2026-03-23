94aaf732fb7a17f9de6072dc0beb1cea10d01835
Salvador Fernández <salvador.fernandez0105@gmail.com>
2026-03-23 16:26:39 -0600

    Ajustes del layout, deseño de cards para proyectos y adicion de traducciones a archivos en.js y es.js

--- src/app/services/language-service.ts
+++ src/app/services/language-service.ts
@@ -1,38 +1,3 @@
-// import { Injectable, inject } from '@angular/core';
-// import { TranslationService, Lang } from './translation-service';
-// import { LocalStorageService } from './local-storage-service';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class LanguageService {
-
-//   private translation = inject(TranslationService);
-//   private storage = inject(LocalStorageService);
-
-//   async init() {
-//     let lang = this.storage.getItem('lang') as Lang;
-
-//     if (!lang) {
-//       lang = this.getBrowserLang();
-//       this.storage.setItem('lang', lang);
-//     }
-
-//     await this.translation.loadLang(lang);
-//   }
-
-//   async setLang(lang: Lang) {
-//     this.storage.setItem('lang', lang);
-//     await this.translation.loadLang(lang);
-//   }
-
-//   private getBrowserLang(): Lang {
-//     const browserLang = navigator.language.split('-')[0];
-//     return browserLang === 'es' ? 'es' : 'en';
-//   }
-
-// }
-
 import { Injectable, inject } from '@angular/core';
 import { TranslationService, Lang } from './translation-service';
 import { LocalStorageService } from './local-storage-service';
