23a79e5331e454788dcdc629f8e50d85193185cd
Salvador Fernández <salvador.fernandez0105@gmail.com>
2026-03-24 09:51:03 -0600

    Delete environment directory

--- environment/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-
-  emailjs: {
-    publicKey: '84KwdsnNF-hM78zbA',     // Puedes usar la misma clave
-    serviceId: 'service_s8cii0z',
-    templateId: 'template_7sebw4d'
-  }
-};
--- environment/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-
-  emailjs: {
-    publicKey: '84KwdsnNF-hM78zbA',
-    serviceId: 'service_s8cii0z',
-    templateId: 'template_7sebw4d'
-  }
-};
