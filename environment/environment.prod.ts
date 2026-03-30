02ae80e3dae623866da1a67414d328ed3e526162
Salvador Fernández <salvador.fernandez0105@gmail.com>
2026-03-30 16:14:40 -0600

    Eliminar la referencia a variables de entorno en .gitignore y agregar archivos de configuración de entorno para producción y desarrollo

---
+++ environment/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+
+  emailjs: {
+    publicKey: '84KwdsnNF-hM78zbA',
+    serviceId: 'service_s8cii0z',
+    templateId: 'template_7sebw4d'
+  }
+};
---
+++ environment/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+
+  emailjs: {
+    publicKey: '84KwdsnNF-hM78zbA',
+    serviceId: 'service_s8cii0z',
+    templateId: 'template_7sebw4d'
+  }
+};
